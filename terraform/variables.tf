bf4a50ae05ae9564758c4b917d14c91c9de7fdaf
031nna <merenuou@gmail.com>
2025-11-28 04:47:33 -0700

    WIP changes on Fri Nov 28 04:47:33 AM MST 2025 :dog:

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -35,36 +35,7 @@ variable "app_domain_name" {
     description = "name of app domain"
     default = "03i.co"
 }
-
-variable "records" {
-  type = list(object({
-    value    = string
-    priority = number
-  }))
-
-  default = [
-    {
-      value    = "ASPMX.L.GOOGLE.COM."
-      priority = 1
-    },
-    {
-      value    = "ALT1.ASPMX.L.GOOGLE.COM."
-      priority = 5
-    },
-    {
-      value    = "ALT2.ASPMX.L.GOOGLE.COM."
-      priority = 5
-    },
-    {
-      value    = "ALT3.ASPMX.L.GOOGLE.COM."
-      priority = 10
-    },
-    {
-      value    = "ALT4.ASPMX.L.GOOGLE.COM."
-      priority = 10
-    }
-  ]
-}
+ 
 
 variable "aws_region" {
   description = "ec2 instance type"
@@ -72,31 +43,12 @@ variable "aws_region" {
   default = "us-east-1"
 }
 
-variable "ami_id" {
-    description = "ami image type eg ubuntu"
-    type = string
-    default = "ami-067cf009aedb2612d"  #"ami-0fc5d935ebf8bc3bc" # "t4g.small"  
-}
-
-variable "instance_type" {
-    description = "instance type"
-    type = string
-    default = "t4g.small" #"t3.micro" #  "ami-067cf009aedb2612d" 
-}
 
 variable "private_key_path" {
   description = "The file path to the private SSH key used to access servers(EC2 instances/DO droplets)"
   type        = string
   default     = "~/.ssh/id_rsa" # Replace with the path to your private key
 }
-
-variable "aws_key_name" {
-  description = "The name of the SSH key pair to use for EC2 instances"
-  type        = string
-  default     = "giggl-staging" # "03i-staging". Replace with your actual key pair name in AWS
-}
-
-variable "ebs_volume_size" {
-  default = 20  # Set the new size (adjust as needed)
-}
+ 
+ 
  
