4e6767f070822d7a679b9384cb721cef3b302bff
031nna <merenuou@gmail.com>
2025-11-29 12:22:50 -0700

    WIP changes on Sat Nov 29 12:22:50 MST 2025 :dog:

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,4 @@
 # Default values
-
-
-
-variable "admin_user" {
-  description = "Nizzles mail"
-}
-
 variable "instance_type" {
   description = "Image identifier of the OS "
   default     = "ml.t2.medium" 
@@ -15,7 +8,6 @@ variable "aws_profile" {
   description = "AWS profile to use"
   default     = "aws_david_dev"
 }
-
 
 # NB: For 30,000 users/day use the equivalent of s-4vcpu-8gb 
 # or betterstill split into two servers (s-2vcpu-4gb x 2)  https://slugs.do-api.dev/
@@ -35,14 +27,12 @@ variable "app_domain_name" {
     description = "name of app domain"
     default = "03i.co"
 }
- 
 
 variable "aws_region" {
   description = "ec2 instance type"
   type = string
   default = "us-east-1"
 }
-
 
 variable "private_key_path" {
   description = "The file path to the private SSH key used to access servers(EC2 instances/DO droplets)"
