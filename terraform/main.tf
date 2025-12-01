54c2b5cad2a291a521b95b4bc7e23e6582fc0bc3
031nna <merenuou@gmail.com>
2025-12-01 05:23:37 -0700

    WIP changes on Mon Dec  1 05:23:37 MST 2025 :dog:

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,33 +16,33 @@ provider "aws" {
 ########################################
 # 1️⃣ VPC + Subnets
 ########################################
-resource "aws_vpc" "davids_vpc" {
+resource "aws_vpc" "my_vpc" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
-  tags = { Name = "davids-vpc" }
+  tags = { Name = "my-vpc" }
 }
 
-resource "aws_subnet" "davids_subnet_a" {
-  vpc_id                  = aws_vpc.davids_vpc.id
+resource "aws_subnet" "my_subnet_a" {
+  vpc_id                  = aws_vpc.my_vpc.id
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
-  tags = { Name = "davids-subnet-a" }
+  tags = { Name = "my-subnet-a" }
 }
 
-resource "aws_subnet" "davids_subnet_b" {
-  vpc_id                  = aws_vpc.davids_vpc.id
+resource "aws_subnet" "my_subnet_b" {
+  vpc_id                  = aws_vpc.my_vpc.id
   cidr_block              = "10.0.2.0/24"
   availability_zone       = "us-east-1b"
   map_public_ip_on_launch = true
-  tags = { Name = "davids-subnet-b" }
+  tags = { Name = "my-subnet-b" }
 }
 
-resource "aws_security_group" "davids_sg" {
-  name        = "davids-sg"
+resource "aws_security_group" "my_sg" {
+  name        = "my-sg"
   description = "Allow SageMaker Studio access"
-  vpc_id      = aws_vpc.davids_vpc.id
+  vpc_id      = aws_vpc.my_vpc.id
 
   ingress {
     from_port   = 443
@@ -90,19 +90,19 @@ resource "aws_iam_role_policy_attachment" "s3_full_access" {
 ########################################
 # 3️⃣ SageMaker Domain
 ########################################
-resource "aws_sagemaker_domain" "davids_domain" {
-  domain_name = "davids-domain"
+resource "aws_sagemaker_domain" "my_domain" {
+  domain_name = "my-domain"
   auth_mode   = "IAM"
-  vpc_id      = aws_vpc.davids_vpc.id
+  vpc_id      = aws_vpc.my_vpc.id
   subnet_ids  = [
-    aws_subnet.davids_subnet_a.id,
-    aws_subnet.davids_subnet_b.id
+    aws_subnet.my_subnet_a.id,
+    aws_subnet.my_subnet_b.id
   ]
   app_network_access_type = "PublicInternetOnly"
 
   default_user_settings {
     execution_role = aws_iam_role.sagemaker_execution_role.arn
-    security_groups = [aws_security_group.davids_sg.id]
+    security_groups = [aws_security_group.my_sg.id]
     jupyter_server_app_settings {
       default_resource_spec {
         instance_type =  "system"
@@ -115,12 +115,12 @@ resource "aws_sagemaker_domain" "davids_domain" {
 # 4️⃣ SageMaker User Profile
 ########################################
 resource "aws_sagemaker_user_profile" "user_dave" {
-  domain_id         = aws_sagemaker_domain.davids_domain.id
+  domain_id         = aws_sagemaker_domain.my_domain.id
   user_profile_name = "dave"
 
   user_settings {
     execution_role  = aws_iam_role.sagemaker_execution_role.arn
-    security_groups = [aws_security_group.davids_sg.id]
+    security_groups = [aws_security_group.my_sg.id]
 
     jupyter_server_app_settings {
       default_resource_spec {
